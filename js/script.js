b6a91c4adb5ede0ace0404d1d0bb59f3466278ef
seron-ux <abyseronkirui@gmail.com>
2020-12-16 11:23:48 +0300

    styled image

---
+++ js/script.js
@@ -0,0 +1,10 @@
+jQuery('h1').click(function(){
+    alert("this is a heaing tag")
+});
+jQuery('p').click(function(){
+    alert("This is a paragraph")
+  });
+
+  jQuery('img').click(function(){
+    alert("This is an image")
+  });
